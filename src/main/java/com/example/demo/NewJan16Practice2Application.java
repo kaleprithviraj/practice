d8c9fc8f68dcd42d671d4a50d5e1361adc277151
prithviraj <prithviraj4053@gmail.com>
2022-01-23 22:07:40 +0530

    commited

---
+++ src/main/java/com/example/demo/NewJan16Practice2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewJan16Practice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewJan16Practice2Application.class, args);
+	}
+
+}
